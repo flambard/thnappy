10ca912251b92190c5b10be748a5f41a585714be
Markus Flambard <markus.flambard@klarna.com>
2011-05-18 20:36:09 +0200

    Actually use the defined conditions.

--- src/package.lisp
+++ src/package.lisp
@@ -5,13 +5,19 @@
   (:use :cl :cffi)
   (:export
 
+   ;; High level functions
    :compress
    :uncompress
    :valid-compressed-p
 
+   ;; Low level functions
    :compress-string
    :compress-byte-vector
    :uncompress-byte-vector
    :valid-compressed-byte-vector-p
+
+   ;; Conditions
+   :invalid-input
+   :buffer-too-small
 
    ))
